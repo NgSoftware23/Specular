f483db9c94c23dd72ed64d1f5a2252e98e3c03a8
NgSoftware23 <nucspb@gmail.com>
2023-05-25 20:50:47 +0300

    Лрбавил валидатор

---
+++ Administrations/Specular.Administrations.Services/Validators/UpdateOrganizationModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using NgSoftware.Specular.Administrations.Services.Contracts.Models.Organizations;
+
+namespace NgSoftware.Specular.Administrations.Services.Validators;
+
+/// <summary>
+/// Валидация <see cref="UpdateOrganizationModel"/>
+/// </summary>
+public class UpdateOrganizationModelValidator : AbstractValidator<UpdateOrganizationModel>
+{
+    /// <summary>
+    /// Инициализирует новый экземпляр <see cref="UpdateOrganizationModelValidator"/>
+    /// </summary>
+    public UpdateOrganizationModelValidator()
+    {
+        RuleFor(x => x.UserId).NotEmpty();
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Description);
+    }
+}
